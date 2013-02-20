1e14af051a09b22bad3181a16dcfc4eb49abcee2
Lucas Ludewigs <lucasludewigs@hotmail.com>
2013-02-20 16:47:22 -0300

    Added function to verify if the string is in the format 'HH:MM:SS' or 'H:MM:SS'

---
+++ timeCheck.py
@@ -0,0 +1,25 @@
+def checkTimeString(string_time):
+    try:
+        split_string = string_time.split(":")
+    except:
+        print "Wrong format, must be a string"
+        return None          
+    if len(split_string) == 3 and 0 < len(split_string[0]) < 3 and len(split_string[1]) == 2 and len(split_string[2]) == 2:
+        #check 'string' hour
+        if  int(split_string[0]) <0 or int(split_string[0])> 23:
+            print "Wrong format, hour must be between 0 and 23"
+            return None
+        if len(split_string[0]) == 1:
+            split_string[0] = "0" + split_string[0]
+        #check 'string' minute
+        if int(split_string[1]) <0 or int(split_string[1])> 59:
+            print "Wrong format, minute must be between 0 and 59"
+            return None
+        #check 'string' minute
+        if int(split_string[2]) <0 or int(split_string[2])> 59:
+            print "Wrong format, second must be between 0 and 59"
+            return None
+    else:
+        print "string must be in format 'HH:MM:SS'"
+        return None
+    return = split_string[0] + ":" + split_string[1] + ":" + split_string[2]
