8705b036e05ab2212ef653e1c99b2f930f8a161c
Heitor Reis <marcheing@gmail.com>
2014-07-23 11:50:36 -0300

    [tests] Add first tests to MySQLConnection
    Connection tests and the first find test without parameters

---
+++ tests/TestMySQLConnection.py
@@ -0,0 +1,38 @@
+import unittest
+import sys
+import os
+sys.path.append('..')
+sys.path.append('../..')
+from MySQLConnection import MySQLConnection
+from django.db import ProgrammingError
+
+
+class MySQLConnectionTest(unittest.TestCase):
+
+    def setUp(self):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'aeSupernova.settings'
+
+    def test_create_connection(self):
+        connection = MySQLConnection()
+        self.assertIsNotNone(connection, 'Connection could not be created')
+
+    def test_execute_query(self):
+        connection = MySQLConnection()
+        query = 'SHOW DATABASES'
+        result = connection.execute(query)
+        self.assertIn((u'supernova',), result,
+                      'Supernova not in databases')
+
+    def test_execute_query_exception(self):
+        connection = MySQLConnection()
+        query = 'SELECT'
+        self.assertRaises(ProgrammingError, connection.execute, (query))
+
+    def test_find_no_parameters(self):
+        connection = MySQLConnection()
+        query = 'SHOW DATABASES'
+        result = connection.find(query, {})
+        self.assertIn((u'supernova',), result)
+
+if __name__ == '__main__':
+    unittest.main()
